77f367eebfe394bebae85ce1962e703c1eaea7e0
rashrayhan <rashrayhan@gmail.com>
2020-03-14 10:03:07 -0500

    resolved conflicts

--- evoting-app/src/app/app-routing.module.ts
+++ evoting-app/src/app/app-routing.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
-<<<<<<< HEAD
-const routes: Routes = [
-
-];
-=======
 const routes: Routes = [{
-  path: "",
-  redirectTo: "evote",
-  pathMatch: "full"
+  path: '',
+  redirectTo: 'evote',
+  pathMatch: 'full'
 },
 {
-  path: "evote",
+  path: 'evote',
   component: AppComponent
 }];
->>>>>>> 9e654c780ef883ee85b34ca0c6889bcaa5f9529a
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
