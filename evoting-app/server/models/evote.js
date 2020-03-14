076b4c0b65e092c5a9737bd16a9f91db4b6a522e
gfasil <fhabtegiorgis@mum.edu>
2020-03-14 16:27:08 -0500

    more modification to schemas.went back to the first schema design to decrease complexity of queries

--- evoting-app/server/models/evote.js
+++ evoting-app/server/models/evote.js
@@ -7,8 +7,31 @@ const electionSchema = mongoose.Schema(
         Name: String,
         state: String,
         population: Number,
+        citizens: [
 
-        'counties_detail': [
+            {
+                'first_name': String,
+                'last_name': String,
+                'drivering_licencse_number': String,
+                DOB: Date,
+                Address: {
+                    zipcode: String,
+                    mail_box: String,
+                    street: String,
+                    house_no: String
+
+
+                },
+                'registered_to_vote': boolean,
+                'voted': boolean,
+                email: String,
+                'phone_number': Number,
+                SSN: Number
+
+            },
+
+        ],
+        counties_detail: [
             {
                 "_id": Number,
                 "name": String,
@@ -18,17 +41,21 @@ const electionSchema = mongoose.Schema(
                         bio: {
                             '_id': Number,
                             name: String, "_id": Number, SSN: Number, picture: String,
-                            address: {
-                                type: mongoose.SchemaTypes.ObjectId, ref: 'Address'
-
+                            Address: {
+                                zipcode: String,
+                                mail_box: String,
+                                street: String,
+                                house_no: String
 
                             },
                             party: {
                                 name: String,
                                 'office_phone': Number,
                                 office_address: {
-                                    type: mongoose.SchemaTypes.ObjectId, ref: 'Address'
-
+                                    zipcode: String,
+                                    mail_box: String,
+                                    street: String,
+                                    house_no: String
 
                                 },
                                 description: String,
@@ -36,9 +63,8 @@ const electionSchema = mongoose.Schema(
                                 'president_name': String
 
                             },
-                            'valid_vote_count': Number,
-                            'invalid_vote_count': Number,
-                            'total_vote_count': Number
+                            'vote_count': Number,
+
 
                         }
                     }
@@ -54,10 +80,13 @@ const electionSchema = mongoose.Schema(
                         fax: Number,
 
                         address: {
-                            type: mongoose.SchemaTypes.ObjectId, ref: 'Address'
-
-
-                        },
+                            zipcode: String,
+                            mail_box: String,
+                            street: String,
+                            house_no: String,
+                            location: [string, string],
+                            email: string
+                        }
                     },
                 ]
 
@@ -70,26 +99,5 @@ const electionSchema = mongoose.Schema(
 
 
 );
-electionSchema.pre('save', function (next) {
-
-    let currentDate = new Date;
-    this.updated_at = currentDate;
-    if (!this.created_at) this.created_at = currentDate;
-    console.log('date set')
-    next();
-
-})
-electionSchema.pre('update', function (next) {
-
-    let currentDate = new Date;
-    this.updated_at = currentDate;
-
-    console.log('date set')
-    next();
-
-})
-
-
-
-modules.exports = mongoose.model('Evote', electionSchema);
+modules.exports = mongoose.model('evote', electionSchema);
 
