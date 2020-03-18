dd02639dfadce89a0666c78f74f12a402fd4337f
Bleink <57928442+Bleink@users.noreply.github.com>
2020-03-18 12:51:15 -0500

    router for the likes added

---
+++ litmovie-app/litmovie-server/routes/like.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { Like } = require("../models/Like");
+const { Dislike } = require("../models/Dislike");
+
+const { auth } = require("../middleware/auth");
+
+//=================================
+//             Likes DisLikes
+//=================================
+
+router.post("/getLikes", (req, res) => {
+
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId }
+    } else {
+        variable = { commentId: req.body.commentId }
+    }
+
+    Like.find(variable)
+        .exec((err, likes) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, likes })
+        })
+
+
+})
+
+
+module.exports = router;
