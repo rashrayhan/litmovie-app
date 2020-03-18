327680ff5b2859316db2d53cdea862714fb94152
Bleink <57928442+Bleink@users.noreply.github.com>
2020-03-18 12:58:03 -0500

    getDislike and upLike added

--- litmovie-app/litmovie-server/routes/like.js
+++ litmovie-app/litmovie-server/routes/like.js
@@ -28,4 +28,44 @@ router.post("/getLikes", (req, res) => {
 })
 
 
+router.post("/getDislikes", (req, res) => {
+
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId }
+    } else {
+        variable = { commentId: req.body.commentId }
+    }
+
+    Dislike.find(variable)
+        .exec((err, dislikes) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, dislikes })
+        })
+
+})
+
+router.post("/upLike", (req, res) => {
+
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId , userId: req.body.userId }
+    }
+
+    const like = new Like(variable)
+    //save the like information data in MongoDB
+    like.save((err, likeResult) => {
+        if (err) return res.json({ success: false, err });
+        //In case disLike Button is already clicked, we need to decrease the dislike by 1 
+        Dislike.findOneAndDelete(variable)
+            .exec((err, disLikeResult) => {
+                if (err) return res.status(400).json({ success: false, err });
+                res.status(200).json({ success: true })
+            })
+    })
+
+})
+
 module.exports = router;
