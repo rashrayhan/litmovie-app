ca560523b4327a3ff84cc2d086e1c7f82f178608
Bleink <57928442+Bleink@users.noreply.github.com>
2020-03-18 13:06:16 -0500

    upDislike and undislike added

--- litmovie-app/litmovie-server/routes/like.js
+++ litmovie-app/litmovie-server/routes/like.js
@@ -68,4 +68,66 @@ router.post("/upLike", (req, res) => {
 
 })
 
+router.post("/unLike", (req, res) => {
+
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId , userId: req.body.userId }
+    }
+
+    Like.findOneAndDelete(variable)
+        .exec((err, result) => {
+            if (err) return res.status(400).json({ success: false, err })
+            res.status(200).json({ success: true })
+        })
+
+})
+
+
+router.post("/unDisLike", (req, res) => {
+
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId , userId: req.body.userId }
+    }
+
+    Dislike.findOneAndDelete(variable)
+    .exec((err, result) => {
+        if (err) return res.status(400).json({ success: false, err })
+        res.status(200).json({ success: true })
+    })
+
+
+})
+
+
+
+router.post("/upDisLike", (req, res) => {
+
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId , userId: req.body.userId }
+    }
+
+    const disLike = new Dislike(variable)
+    //save the like information data in MongoDB
+    disLike.save((err, dislikeResult) => {
+        if (err) return res.json({ success: false, err });
+        //In case Like Button is already clicked, we need to decrease the like by 1 
+        Like.findOneAndDelete(variable)
+            .exec((err, likeResult) => {
+                if (err) return res.status(400).json({ success: false, err });
+                res.status(200).json({ success: true })
+            })
+    })
+
+
+})
+
 module.exports = router;
